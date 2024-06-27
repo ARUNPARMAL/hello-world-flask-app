68b875bd64c7f7b7e651313af2596286cddd9b4d
Arun <arun211202@gmail.com>
2024-06-27 11:28:26 +0530

    removed waitress

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-# app.py
 from flask import Flask
+# from waitress import serve
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
     return "Hello, Azure Web Apps!"
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=5000)  
